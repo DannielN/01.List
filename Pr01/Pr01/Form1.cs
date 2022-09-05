1a3fdb82569a9b849e6709ee90aab0a4c7e597b7
novacekd.05 <novacekd.05@A222-02>
2022-09-05 09:13:10 +0200

    Pr01

--- Pr01/Pr01/Form1.cs
+++ Pr01/Pr01/Form1.cs
@@ -16,5 +16,43 @@ namespace Pr01
         {
             InitializeComponent();
         }
+
+
+        List<int> list = new List<int>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(textBox1.Text);
+            Random rnd = new Random();
+
+            for (int k = 0; k < n; k++)
+            {
+                list.Add(rnd.Next(1, 101));
+                listBox1.Items.Add(list[k]);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            list.Sort();
+            string text = string.Empty;
+
+            for (int k = 0; k < list.Count(); k++)
+            {
+                text += list[k];
+                text += ",";
+            }
+            MessageBox.Show(text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            list.Clear();
+            listBox1.Items.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
