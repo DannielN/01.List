e99cf0516fe25a36c6048eac88a39cd5bfa8b7f7
novacekd.05 <novacekd.05@A222-02>
2022-09-22 09:25:12 +0200

    Pr02/03

---
+++ Pr01/Knihovna/Knih.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Knihovna
+{
+    public class Knih
+    {
+        public static void Vypis(ListBox Lis, List<int> list)
+        {
+            int n = list.Count();
+            for (int k = 0; k < n; k++)
+            {
+                Lis.Items.Add(list[k]);
+            }
+        }
+
+        public static List<int> Smazani(int k, List<int> list)
+        {
+            while (list.Contains(k))
+            {
+                list.Remove(k);
+            }
+            return list;
+        }
+
+        public static List<int> SmazaniLam(List<int> list, int k)
+        {
+            list.RemoveAll(a => a == k);
+            return list;
+        }
+
+        public static int DruheMax(List<int> list, out int poradi)
+        {
+            int SecondMax = -6;
+            poradi = -1;
+            int max = -6;
+            int k = list.Count();
+
+            for (int i = 0; i < k; i++)
+            {
+                if (list[i] > max)
+                {
+                    SecondMax = max;
+                    max = list[i];
+                }
+                else if (list[i] < max && list[i] > SecondMax)
+                {
+                    SecondMax = list[i];
+                    poradi = i;
+                }
+            }
+
+            return SecondMax;
+        }
+    }
+}
---
+++ Pr01/Pr02/Form1.cs
@@ -0,0 +1,67 @@
+﻿using Knihovna;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pr02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        List<int> list = new List<int>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            list.Clear();
+
+            Random rnd = new Random();
+            int n = Convert.ToInt32(textBox1.Text);
+            
+            for (int k = 0; k < n; k++)
+            {
+                list.Add(rnd.Next(-5, 21));
+            }
+
+            Knih.Vypis(listBox1, list);
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            int k = Convert.ToInt32(textBox2.Text);
+
+            Knih.Smazani(k, list);
+            Knih.Vypis(listBox2, list);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            int k = Convert.ToInt32(textBox2.Text);
+
+            Knih.SmazaniLam(list, k);
+            Knih.Vypis(listBox2, list);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int k = Convert.ToInt32(textBox2.Text);
+
+            int SecondMax = Knih.DruheMax(list, out int poradi);
+            
+            MessageBox.Show("Druhe maximum je " + SecondMax + " jeho index je " + poradi);
+        }
+    }
+}
